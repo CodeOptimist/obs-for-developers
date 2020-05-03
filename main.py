6897117db73b32f08cd9e973eb6e6d5df1b3d730
Christopher Galpin <52485+CodeOptimist@users.noreply.github.com>
2020-05-03 08:18:48 -0600

    fix memory crashes (yay for reading documentation)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json, re
+import json, re, yaml, os
 if __name__ != '__main__':
     import obspython as obs
 from autohotkey import Script, AhkExitException
@@ -7,19 +7,19 @@ from collections import namedtuple
 from win32api import OutputDebugString
 
 
+os.chdir(r'C:\Dropbox\Python\obs')
 ahk = Script()
 AhkWindow = namedtuple('AhkWindow', ['ahk_title', 'title', 'class_', 'exe', 'is_regex'])
 video_info = None
-scene = None
+loaded = None
+windows = None
 
-windows = {
-    'Visual Studio': {'spec': "RimMods - Microsoft Visual Studio:HwndWrapper[DefaultDomain;;f1776b62-97a2-4920-9344-4a8e003b5404]:devenv.exe"},
-    'dnSpy': {'spec': "dnSpy v6.0.5 (64-bit):HwndWrapper[dnSpy.exe;;dcb937d0-a05d-4507-8a73-c965d495a0ce]:dnSpy.exe"},
-    'TortoiseHg Workbench': {'spec': "JobsOfOpportunity - TortoiseHg Workbench:Qt5QWindowIcon:thgw.exe"},
-    'TortoiseHg Commit': {'spec': "JobsOfOpportunity - commit:Qt5QWindowIcon:thgw.exe"},
-    'RimWorld': {'spec': "RimWorld by Ludeon Studios:UnityWndClass:RimWorldWin64.exe"},
-    'BC Text Compare': {'spec': "/.*@.* <--> .*@.* - Text Compare - Beyond Compare/:TViewForm:BCompare.exe"},
-}
+
+def init():
+    global loaded, windows
+    with open(r'captures.yaml', encoding='utf-8') as f:
+        loaded = yaml.safe_load(f)
+    #windows = loaded['scenes']['Jobs of Opportunity']
 
 
 def log(func):
@@ -49,8 +49,8 @@ def timer():
             ahk_window = get_ahk_window(window['spec'])
 
             if ahk.f('WinActiveRegEx', ahk_window.ahk_title, ahk_window.is_regex):
-                scene_item = obs.obs_scene_find_sceneitem_by_id(scene, window['sceneitem_id'])
-                source = obs.obs_sceneitem_get_source(scene_item)
+                sceneitem = obs.obs_scene_find_sceneitem_by_id(scene, window['sceneitem_id'])
+                source = obs.obs_sceneitem_get_source(sceneitem)
 
                 ahk.call('ActiveWin')
                 spec = ":".join([(ahk.get('title').replace(':', '#3A')), ahk.get('class'), ahk_window.exe])
@@ -62,10 +62,8 @@ def timer():
                     obs.timer_add(lambda: update_source(source, spec=spec) or obs.remove_current_callback(), 2500)
                 window['state'] = state
 
-                # obs.obs_source_release(source)
-                center_item(scene_item, ahk_window)
-                obs.obs_sceneitem_set_order_position(scene_item, len(windows) - 1)
-                # obs.obs_sceneitem_release(scene_item)
+                center_item(sceneitem, ahk_window)
+                obs.obs_sceneitem_set_order_position(sceneitem, len(windows) - 1)
     except AhkExitException as ex:
         obs.timer_remove(timer)
         print(ex)
@@ -96,8 +94,79 @@ def center_item(item, ahk_window):
 
 
 @log
+def wipe_scenes():
+    sources = obs.obs_frontend_get_scenes()
+    for idx, source in enumerate(sources):
+        if idx == 0:
+            continue
+        obs.obs_source_remove(source)
+    obs.source_list_release(sources)
+
+
+@log
+def get_scene_by_name(scene_name):
+    try:
+        sources = obs.obs_frontend_get_scenes()
+        for source in sources:
+            name = obs.obs_source_get_name(source)
+            if name == scene_name:
+                return obs.obs_scene_from_source(source)
+        return None
+    finally:
+        obs.source_list_release(sources)
+
+
+@log
+def wipe_scene(scene):
+    items = obs.obs_scene_enum_items(scene)
+    for item in items:
+        obs.obs_sceneitem_remove(item)
+        source = obs.obs_sceneitem_get_source(item)
+        obs.obs_source_remove(source)
+    obs.sceneitem_list_release(items)
+
+
+@log
+def scenes_loaded():
+    scene_names = obs.obs_frontend_get_scene_names()
+    if not scene_names:
+        return
+    obs.remove_current_callback()
+
+    for scene_name, scene_windows in loaded['scenes'].items():
+        scene = get_scene_by_name(scene_name)
+
+        create_scene = scene is None
+        if create_scene:
+            scene = obs.obs_scene_create(scene_name)
+        else:
+            wipe_scene(scene)
+
+        for window_name, window in scene_windows.items():
+            source_info = {'id': 'window_capture', 'name': window_name, 'settings': {'method': 2, 'priority': 1, 'window': window['spec']}}
+            data = obs.obs_data_create_from_json(json.dumps(source_info))
+            source = obs.obs_load_source(data)
+            obs.obs_data_release(data)
+
+            sceneitem = obs.obs_scene_add(scene, source)
+            obs.obs_source_release(source)
+            window['sceneitem_id'] = obs.obs_sceneitem_get_id(sceneitem)
+            obs.obs_sceneitem_set_locked(sceneitem, True)
+
+            ahk_window = get_ahk_window(window['spec'])
+            center_item(sceneitem, ahk_window)
+
+        if create_scene:
+            obs.obs_scene_release(scene)
+
+    #obs.timer_add(timer, 50)
+
+
+@log
 def script_load(settings):
-    global ahk, scene, video_info
+    global ahk, video_info
+    init()
+
     if ahk is not None:
         ahk.exit()
 
@@ -110,44 +179,19 @@ def script_load(settings):
     video_info = obs.obs_video_info()
     obs.obs_get_video_info(video_info)
 
-    @log
-    def wipe_scene():
-        items = obs.obs_scene_enum_items(scene)
-        for item in items:
-            obs.obs_sceneitem_remove(item)
-            source = obs.obs_sceneitem_get_source(item)
-            obs.obs_source_remove(source)
-            obs.obs_source_release(source)
-            obs.obs_sceneitem_release(item)
-        obs.sceneitem_list_release(items)
+    obs.timer_add(scenes_loaded, 50)
 
-    scene = obs.obs_scene_from_source(obs.obs_get_source_by_name("Scene 1"))
-    wipe_scene()
 
-    for name, window in windows.items():
-        source_info = {'id': 'window_capture', 'name': name, 'settings': {'method': 2, 'priority': 1, 'window': window['spec']}}
-        data = obs.obs_data_create_from_json(json.dumps(source_info))
-        source = obs.obs_load_source(data)
-        obs.obs_data_release(data)
-
-        scene_item = obs.obs_scene_add(scene, source)
-        obs.obs_source_release(source)
-        window['sceneitem_id'] = obs.obs_sceneitem_get_id(scene_item)
-        obs.obs_sceneitem_set_locked(scene_item, True)
-
-        ahk_window = get_ahk_window(window['spec'])
-        center_item(scene_item, ahk_window)
-        obs.obs_sceneitem_release(scene_item)
-
-    obs.timer_add(timer, 50)
 
 
 @log
 def script_unload():
-    obs.obs_scene_release(scene)
     ahk.exit()
 
 
 def script_description():
     return "Python & AutoHotkey powered OBS."
 
+
+if __name__ == '__main__':
+    init()
